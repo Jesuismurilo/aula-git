9c7f316fe129a10a152c632d432d98a4e13e3f48
Jesuismurilo <murilosilvadejesus02@gmail.com>
2024-08-19 22:26:53 -0300

    Create valor.js

---
+++ valor.js
@@ -0,0 +1,17 @@
+//Elabore um programa que:
+// Solicite o nome de um produto
+// Solicite a quantidade vendida
+// Solicite o valor do produto
+// Apresente o valor total
+// Solicite a %  de desconto
+// Apresente o R$ total de desconto
+// Apresente o valor total da venda depois do desconto
+
+
+var produto = parseFloat(prompt("Digite o produto:"));
+var quantidade = parseFloat(prompt("Digite a quantidade vendida:"));
+var valor = parseFloat(prompt("Digite o valor do produto:"));
+var valorTotal = Quantidade*valor;
+var valor = parseFloat(prompt("Digite o valor a converter:"));
+
+
