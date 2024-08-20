77a68532b8b62cda21d3246f66986a6fc53b9b47
Jesuismurilo <murilosilvadejesus02@gmail.com>
2024-08-19 22:10:27 -0300

    valor

---
+++ porcentagem.js
@@ -0,0 +1,9 @@
+var valor = parseFloat(prompt("Digite o valor a converter:"));
+var porcentagem = parseInt(prompt("Digite a porcentagem encontrar:"));
+// Converte o numero inteiro em decimal
+// Só é possivel achar a % usando numero decimal
+var porcentagemDecimal = porcentagem /100;
+// Multiplica o valor pela porcentagem para encontrar o resultado
+var resultado = valor * porcentagemDecimal;
+
+console.log ("O Resultado é: " + resultado);
