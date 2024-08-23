eed931875dde5bf4e850c131d0f9e9725ecb989b
Jesuismurilo <murilosilvadejesus02@gmail.com>
2024-08-22 22:26:31 -0300

    correção

--- Vendas.js
+++ Vendas.js
@@ -13,7 +13,7 @@ var quantidadeVendida = parseInt (prompt ("Digite a quantidade vendida:"));
 var valorProduto = parseFloat (prompt("Digite o valor do produto:"));
 var totalVenda = quantidadeVendida*valorProduto;
 console.log ( "O total da venda é" + totalVenda );
-var porcentagemdeDesconto = parseInt (prompt("Digite a porcentagem de desconto"));
+var porcentagemDesconto = parseInt (prompt("Digite a porcentagem de desconto"));
 var porcentagemDecimal =  porcentagemDesconto /100;
 var descontoReais = totalVenda + porcentagemDecimal;
 var totalDesconto = totalVenda - descontoReais;
